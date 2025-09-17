a0f2523b51c0e1cb13e4fa39668192ee4cda5c29
konpeitox <konpeito62@gmail.com>
2025-09-17 22:37:14 +0900

    ogp画像の追加・反映

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,33 @@ export const metadata: Metadata = {
   title: "Lumos | 横浜国立大学プログラミングサークル",
   description:
     "横浜国立大学のプログラミングサークル「Lumos」の公式サイトです。初心者から経験者まで、学習・交流を行っています。",
-    generator: "Next.js",
-    icons: {
-    icon: "/favicon.ico", // ここにファビコンのパス
+  generator: "Next.js",
+  icons: {
+    icon: "/favicon.ico",
+  },
+
+  openGraph: {
+    type: "website",
+    title: "Lumos | 横浜国立大学プログラミングサークル",
+    description:
+      "横浜国立大学のプログラミングサークル「Lumos」の公式サイトです。初心者から経験者まで、学習・交流を行っています。",
+    siteName: "lumosweb",
+    url: "https://lumos-ynu.jp/",
+    images: [
+      {
+        url: "https://lumos-ynu.jp/assets/LumosOGP.png",
+        type: "image/png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Lumos | 横浜国立大学プログラミングサークル",
+    description:
+      "横浜国立大学のプログラミングサークル「Lumos」の公式サイトです。初心者から経験者まで、学習・交流を行っています。",
+    images: ["https://lumos-ynu.jp/assets/LumosOGP.png"],
   },
   keywords: ['横国', '横浜国立大学', 'Lumos', 'ルーモス', 'プログラミング', 'サークル', '国大', 'YNU', '初心者', '情報', 'IT'],
 }
