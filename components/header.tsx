b0041e2c0a6b401f7980117cf3f6b0c6c2ef0773
Tivo0921 <105437349+Tivo0921@users.noreply.github.com>
2026-04-07 17:44:41 +0900

    feat: ヘッダーナビゲーションに Members ページへのリンクを追加 (#91) (#138)

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ import { cn } from "@/lib/utils";
 const navigation = [
   { name: "ホーム", href: "/" },
   { name: "サークル紹介", href: "/about" },
-  // { name: "メンバー", href: "/members" },
+  { name: "メンバー", href: "/members" },
   { name: "プロジェクト紹介", href: "/projects" },
   { name: "お知らせ", href: "/news" },
   // { name: "お問い合わせ", href: "/contact" },
