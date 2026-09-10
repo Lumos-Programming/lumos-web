8f961dc72180819e4891730314be39425d69e57c
Shumi1225 <sho12miya@gmail.com>
2026-09-10 22:09:34 +0900

    Fix lumos-webを サークルサイトという単語に統一

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -45,7 +45,7 @@ const currentProjects = [
     id: 4,
     title: "モブプロ",
     description:
-      "Lumosのメンバー全員で、lumos-web の開発に取り組むモブプログラミングを9月に開催します。実際にコードを書きながら、開発の進め方やコードレビューの方法を学び、メンバー全員が開発とレビューに参加できる状態を目指します。",
+      "Lumosのメンバー全員で、サークルサイトの開発に取り組むモブプログラミングを9月に開催します。実際にコードを書きながら、開発の進め方やコードレビューの方法を学び、メンバー全員が開発とレビューに参加できる状態を目指します。",
     image: "/assets/Lumoslogo.png",
     status: "進行中",
     technologies: ["next.js", "typescript", "css", "firebase", "react"],
